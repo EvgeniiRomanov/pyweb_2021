fb1817c75b915f79a850e16a4e16918d1aeba925
EvgeniiRomanov <e78rus@mail.ru>
2022-05-13 14:07:45 +0000

    STATIC_ROOT

--- pyweb_2021/settings.py
+++ pyweb_2021/settings.py
@@ -32,6 +32,7 @@ DEBUG = os.getenv('DEBUG') == 'True'
 ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS').split(',')]
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -124,9 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
